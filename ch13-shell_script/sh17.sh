38fa6a1d75c33f747e6b3ae4a439bfb87f05e9ce
Joyounger <942510346@qq.com>
2017-12-10 08:20:29 +0800

    sh17 v2
    Signed-off-by: Joyounger <942510346@qq.com>

--- ch13-shell_script/sh17.sh
+++ ch13-shell_script/sh17.sh
@@ -6,8 +6,14 @@
 #PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 #export PATH
 
-network="192.168.1."
-for sitenu in $(seq 1 100)
+network="192.168.0."
+for sitenu in $(seq 1 255) 	# seq为连续缩写之意
 do
-	ping $network$sitenu
+	#ping $network$sitenu
+	ping -c 1 -w 1 $network$sitenu &> /dev/null && result=0 || result=1 	#ping执行成功就执行result=0,执行失败就执行result=1
+	if [ "$result" == 0 ]; then
+		echo "$server $network$sitenu is UP"
+	elif [ "$result" == 1 ]; then
+		echo "$server $network$sitenu is DOWN"
+	fi
 done
